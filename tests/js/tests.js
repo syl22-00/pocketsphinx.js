eb4c58738c5e1a5250bdb3e4cbceccdda3b38a9f
Sylvain Chevalier <sylvain.chevalier@gmail.com>
2013-07-27 07:02:28 +0800

    More tests in test suite

--- tests/js/tests.js
+++ tests/js/tests.js
@@ -1,12 +1,4 @@
-
-test( "Initial test", function() {
-    var x = new Module.Recognizer();
-    ok(x != undefined, "Recognizer instantiated successfully" );
-    x.delete();
-});
-
 module("Data structures");
-
 test( "ReturnType type", function() {
     var x = Module.ReturnType.SUCCESS;
     ok (x != undefined, "valid ReturnType should not be undefined" );
@@ -29,7 +21,6 @@ test( "ReturnType type", function() {
     ok ( Module.ReturnType.HELLO == undefined, "Invalid ReturnType should be undefined");
 });
 
-
 test( "VectorWords type", function() {
     var x = new Module.VectorWords();
     ok (x != undefined, "vectorwords should not be undefined");
@@ -42,8 +33,88 @@ test( "VectorWords type", function() {
     equal(x.get(0)[0], "1", "vectorwords element should have the correct value");
     equal(x.get(0)[1], "2", "vectorwords element should have the correct value");
     x.set(1,["3","4"]);
-    equal(x.get(1), undefined, "vectorwords elements be undefined");
+    equal(x.get(1), undefined, "vectorwords elements should be undefined");
 });
+
+test( "Config type", function() {
+    var x = new Module.Config();
+    ok (x != undefined, "config should not be undefined");
+    equal(x.size(), 0, "config should be initialized empty");
+    x.push_back(["",""]);
+    equal(x.size(), 1, "config size should grow");
+    equal(x.get(0)[0], "", "config element should have the correct value");
+    equal(x.get(0)[1], "", "config element should have the correct value");
+    x.set(0,["1","2"]);
+    equal(x.get(0)[0], "1", "config element should have the correct value");
+    equal(x.get(0)[1], "2", "config element should have the correct value");
+    x.set(1,["3","4"]);
+    equal(x.get(1), undefined, "config elements should be undefined");
+});
+
+test( "Transitions types", function() {
+    var x = new Module.VectorTransitions();
+    ok (x != undefined, "vector transitions should not be undefined");
+    equal(x.size(), 0, "vector transitions should be initialized empty");
+    var error = undefined;
+    try {x.push_back({});}
+    catch (e) {error = e;}
+    ok(error != undefined, "Adding an invalid value to vector should raise an exception");
+    equal(error.name, "TypeError", "Should be a TypeError exception");
+
+    error = undefined;
+    try {x.push_back({from: "", to: "", hello: ""});}
+    catch (e) {error = e;}
+    ok(error != undefined, "Adding an invalid value to vector should raise an exception");
+    equal(error.name, "TypeError", "Should be a TypeError exception");
+
+    error = undefined;
+    try {x.push_back(["", "", ""]);}
+    catch (e) {error = e;}
+    ok(error != undefined, "Adding an invalid value to vector should raise an exception");
+    equal(error.name, "TypeError", "Should be a TypeError exception");
+
+    error = undefined;
+    try {x.push_back(undefined);}
+    catch (e) {error = e;}
+    ok(error != undefined, "Adding an invalid value to vector should raise an exception");
+    equal(error.name, "TypeError", "Should be a TypeError exception");
+
+
+    error = undefined;
+    try {x.push_back({from: "", to: 2, word: ""});}
+    catch (e) {error = e;}
+    ok(error != undefined, "Adding an invalid value to vector should raise an exception");
+    equal(error.name, "TypeError", "Should be a TypeError exception");
+
+    error = undefined;
+    try {x.push_back({from: 0, to: 1, word: 2});}
+    catch (e) {error = e;}
+    ok(error != undefined, "Adding an invalid value to vector should raise an exception");
+    equal(error.name, "BindingError", "Should be a BindingError exception");
+
+    x.push_back({from: 0, to: 0, word: ""});
+    equal(x.size(), 1, "config size should grow");
+    x.push_back({from: 0, to: 0, word: ""});
+    x.push_back({from: 0, to: 0, word: ""});
+    x.push_back({from: 0, to: 0, word: ""});
+    x.push_back({from: 0, to: 0, word: ""});
+    x.push_back({from: 0, to: 0, word: ""});
+    x.push_back({from: 0, to: 0, word: ""});
+    x.push_back({from: 0, to: 0, word: ""});
+    x.push_back({from: 0, to: 0, word: ""});
+    x.push_back({from: 0, to: 0, word: ""});
+    x.push_back({from: 0, to: 0, word: ""});
+    equal(x.size(), 11, "config size should grow");
+
+    x.push_back({from: 12, to: 13, word: "hello, world!", hello: 0});
+    equal(x.size(), 12, "Extra fields in transition should not matter");
+    equal(x.get(11).from, 12, "stored transitions should have the correct value");
+    equal(x.get(11).to, 13, "stored transitions should have the correct value");
+    equal(x.get(11).word, "hello, world!", "stored transitions should have the correct value");
+    equal(x.get(11).hello, undefined, "stored transitions should have the correct value");
+    
+});
+
 var recognizer;
 
 module( "With living recognizer", {
